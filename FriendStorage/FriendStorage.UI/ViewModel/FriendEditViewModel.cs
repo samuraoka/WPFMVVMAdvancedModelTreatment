08385b50506cf64eacd11ae1bfa8c36b02c1d2b6
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-06-06 10:39:58 +0900

    Implemented the FriendEditView.

--- FriendStorage/FriendStorage.UI/ViewModel/FriendEditViewModel.cs
+++ FriendStorage/FriendStorage.UI/ViewModel/FriendEditViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using FriendStorage.Model;
+using FriendStorage.UI.Command;
 using FriendStorage.UI.DataProvider;
+using FriendStorage.UI.DataProvider.Lookups;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Windows.Input;
 
 namespace FriendStorage.UI.ViewModel
 {
@@ -12,25 +15,24 @@ namespace FriendStorage.UI.ViewModel
         Friend Friend { get; }
     }
 
-
-    //TODO
     internal class FriendEditViewModel : Observable, IFriendEditViewModel
     {
         //TODO
         private readonly IFriendDataProvider _friendDataProvider;
         //TODO
         private Friend _friend;
-
-        //TODO
+        private IEnumerable<LookupItem> _friendGroups;
+        private FriendEmail _selectedEmail;
 
         public FriendEditViewModel(IFriendDataProvider friendDataProvider) //TODO
         {
             //TODO
             _friendDataProvider = friendDataProvider;
             //TODO
+            AddEmailCommand = new DelegateCommand(OnAddEmailExecute);
+            //TODO
         }
 
-        //TODO
         public void Load(int? friendId = null)
         {
             //TODO
@@ -56,14 +58,50 @@ namespace FriendStorage.UI.ViewModel
             }
         }
 
+        public IEnumerable<LookupItem> FriendGroupLookup
+        {
+            get { return _friendGroups; }
+            set
+            {
+                _friendGroups = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public FriendEmail SelectedEmail
+        {
+            get { return _selectedEmail; }
+            set
+            {
+                _selectedEmail = value;
+                OnPropertyChanged();
+                //TODO
+            }
+        }
+
+        public ICommand SaveCommand { get; private set; }
+
+        public ICommand ResetCommand { get; private set; }
+
+        public ICommand DeleteCommand { get; private set; }
+
+        public ICommand AddEmailCommand { get; private set; }
+
+        public ICommand RemoveEmailCommand { get; private set; }
+
+        //TODO
+
+        private void OnAddEmailExecute(object obj)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
         private void InvalidateCommands()
         {
             //TODO
             Debug.WriteLine("[{0}] InvalidateCommands: Implemente This method"
                 , DateTime.Now);
         }
-
     }
-
-    //TODO
 }
